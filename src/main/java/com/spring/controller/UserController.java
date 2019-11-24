f5045aad529e5fb2435f4503d1430ea1b43e9d19
sanjaybankar12 <sanjaybankar12@gmail.com>
2019-11-24 11:49:53 +0530

    test2 method

--- src/main/java/com/spring/controller/UserController.java
+++ src/main/java/com/spring/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+	@GetMapping(value="/test2")
+	public void test2() {
+		
+	}
 }
